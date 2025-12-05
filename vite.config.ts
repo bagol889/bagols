6d5549ce46eb9ffecceacdc53af3f250d9a36ef8
bagol888 <bagasfuxxy@gmail.com>
2025-12-05 15:44:03 +0700

    Fix Fatal: Rename file extensions to TSX and fix config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Konfigurasi agar process.env bisa dibaca di browser
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    plugins: [react()],
-    define: {
-      'process.env': env
-    }
-  }
+export default defineConfig({
+  plugins: [react()],
+  build: { outDir: 'dist' }
 })
